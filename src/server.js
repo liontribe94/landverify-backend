c37fdb458b3f38f862b5b3f5d051103a2ce15fea
liontribe94 <chinwokenagu@gmail.com>
2025-06-09 12:12:49 +0100

    Update server port to 3003

--- src/server.js
+++ src/server.js
@@ -45,7 +45,7 @@ app.use((err, req, res, next) => {
   });
 });
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 3003;
 
 // Database connection and server start
 const startServer = async () => {
